d2616bc7d1d014db6a1c0f54ae8db3d24ff7936d
LiuChao <893081892@qq.com>
2017-08-14 17:20:39 +0800

    小修改
    文件夹修改和注释修改

--- Animation-GroupDemo/LCRotateAnimation/LCSubItem.h
+++ Animation-GroupDemo/LCRotateAnimation/LCSubItem.h
@@ -1,6 +1,6 @@
 //
-//  LCSubItem.h
-//  Animation-ItemDemo
+//  代码地址:https://github.com/SupermanChao/CATransaction-Demo
+//  简书地址:http://www.jianshu.com/u/d6fe286d5fad
 //
 //  Created by 刘超 on 2017/8/2.
 //  Copyright © 2017年 ogemray. All rights reserved.
--- Animation-GroupDemo/LCRotateAnimation/UIImage+Draw.h
+++ Animation-GroupDemo/LCRotateAnimation/UIImage+Draw.h
@@ -1,6 +1,6 @@
 //
-//  UIImage+Draw.h
-//  Animation-ItemDemo
+//  代码地址:https://github.com/SupermanChao/CATransaction-Demo
+//  简书地址:http://www.jianshu.com/u/d6fe286d5fad
 //
 //  Created by 刘超 on 2017/8/2.
 //  Copyright © 2017年 ogemray. All rights reserved.
